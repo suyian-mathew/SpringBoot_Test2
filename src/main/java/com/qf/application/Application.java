e92e5ca6907f742f32d0b0fe4c87a10956cca2b9
suyian <347933801@qq.com>
2019-09-27 11:31:12 +0800

    东奔西走

---
+++ src/main/java/com/qf/application/Application.java
@@ -0,0 +1,24 @@
+package com.qf.application;
+
+
+import com.qf.application.filter.MyFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    @Bean
+    public FilterRegistrationBean getFilterBean(){
+        FilterRegistrationBean bean = new FilterRegistrationBean();
+        bean.setName("superFilter");
+        bean.setFilter(new MyFilter());
+        bean.addUrlPatterns("/*");
+        return bean;
+    }
+}
---
+++ src/main/java/com/qf/application/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.qf.application.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("stu")
+public class TestController {
+
+    @RequestMapping("test")
+    @ResponseBody
+    public String test(){
+        System.out.println("有点吊的过滤器");
+        return "一点一滴";
+    }
+}
